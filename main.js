5a0e6699e81c017a7115c23337fe10528c38c4ab
adumitroaeicristina <66757600+adumitroaeicristina@users.noreply.github.com>
2020-08-15 21:17:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+"use strict";
+
+//1. display in the console the numbers from 1 to 20
+
+console.log('Afiseaza toate numerele de la 1 la 20: ');
+var x = 1;
+while (x < 21) {
+console.log(x);
+x++;
+}
+
+
+//2. display in the console the odd numbers from 1 to 20
+
+console.log('Afiseaza numerele impare de la 1 la 20: ');
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+
+//3. compute the sum of the elements of an array and display it in the console
+
+let arr = [4, 7, 12, 0, 15, 44, 82, 5];
+let sum = 0;
+for (var i = 0; i < arr.length; i++) {
+     sum = sum + arr[i];
+}
+console.log('Suma elementelor din sir este: ' + sum);
+
+//4. compute the maximum of the elements of an array and display it in the console
+
+let arr1 = [4, 7, 12, 0, 15, 44, 82, 5];
+let max = arr[0];
+for (var i = 0; i < arr1.length; i++) {
+     if (arr1[i] < arr1[i + 1]){
+        max = arr1[i+1];
+     }
+}
+console.log('Elementul cel mai mare din sir este: ' + max);
+
+//5. compute how many times a certain element appears in an array
+
+let nrApp = 0;
+let elem = 5;
+let arr2 = [4, 8, 80, 5, 6, 2, 5];
+console.log("Sirul in care se efectueaza cautarea este: " + arr2);
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] == elem) {
+        nrApp += 1;
+    }    
+}
+console.log("Nr. aparitiilor lui " + elem + " in sir este: " + nrApp);
+
+//6. Challenge
+
+let a = new Array(4);
+for (let i = 0; i < 4; i++) {
+    a[i] = new Array(4);
+    for (let j =0; j < 4; j++){
+       if ((i % 2 == 0) && (j % 2 == 0)){
+           a[i][j] = 0;
+        } else { 
+            if ((i % 2 == 1) && (j % 2 == 1)) {
+                a[i][j] = 0;
+            } else {
+                    a[i][j] = 1;
+                   }
+       }
+    }
+}
+console.log(a);
+
